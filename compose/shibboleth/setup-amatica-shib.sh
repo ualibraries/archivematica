8c8618f2f979215e8565792c9145f772741d013f
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-02-27 13:15:01 -0700

    Sorting output of controlling environment variables

--- compose/shibboleth/setup-amatica-shib.sh
+++ compose/shibboleth/setup-amatica-shib.sh
@@ -109,7 +109,7 @@ function docker_compose_up {
   export AMATICA_DAT_DIR=${AMATICA_DAT_DIR:-"$PERSISTANT_DIR/archivematica"}
   export MYSQL_DAT_DIR=${MYSQL_DAT_DIR:-"$PERSISTANT_DIR/mysql"}
 
-  printenv | grep -e "AMATICA\|FILESENDER\|SMTP_\|MYSQL_\|DAT_DIR\|LOG_DIR"
+  printenv | grep -e "AMATICA\|FILESENDER\|SMTP_\|MYSQL_\|DAT_DIR\|LOG_DIR" | sort
   echo
   
   docker-compose up -d
