f9d5b49fa4666e601b31a46b0fbba0bb96144ea7
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-06-26 14:45:06 -0700

    Updated to checkout compose/overlay as compose/artefactual-labs

--- dbuild.sh
+++ dbuild.sh
@@ -5,65 +5,14 @@ DEFAULT_CONTAINER="amatica"
 
 CONTAINER=${4:-$DEFAULT_CONTAINER}
 REPOSITORY=${3:-archivematica}
-TAG=${2:-1.6.1-beta3}
+TAG=${2:-1.7.1}
 ACTION=${1:-BUILD}
 DAEMONIZE=-d
+DBUILD_SCRIPTS=`ls dbuild-*.sh`
 
-# Delete test container built from docker file
-docker stop $CONTAINER
-docker rm $CONTAINER
+git submodule update --init --recursive
 
-if [ "$ACTION" = "BUILD" ]; then
-# Delete test image built from docker file
-docker image rm $REPOSITORY:$TAG
-
-# Create test image from docker file
-docker build --rm=true -t $REPOSITORY:$TAG . || exit 1
-
-ACTION=DEBUG
-
-fi
-
-if [ "$ACTION" = "DEBUG" ]; then
-    DAEMONIZE=""
-    DEBUG="--user root -it --entrypoint /bin/bash"
-fi
-
-if [ "$ACTION" = "INSTALL" ]; then
-
-HOSTLOG=/tmp/log
-NETWORK="private-network"
-
-docker run $DAEMONIZE $DEBUG \
-       --net=$NETWORK \
-       -p 80:80 \
-       -p 443:443 \
-       -p 8001:8001 \
-       --name $CONTAINER \
-       $REPOSITORY:$TAG
-
-#       --read-only \
-#       --tmpfs /tmp/ \
-#       --tmpfs /var/cache/nginx:mode=755,noexec,nodev,nosuid \
-
-else
-
-#       -e CHOWN_ELASTICSEARCH=320:320 \
-#       -e CHOWN_ELASTICSEARCH=320:320 \
-    
-    docker run $DAEMONIZE $DEBUG \
-       --net=compose_default \
-       -p 80:80 \
-       -p 8000:8000 \
-       -e SMTP_DOMAIN=email.arizona.edu \
-       -e SMTP_HOST=smtpgate.mail.arizona.edu \
-       -e SMTP_FROM=admin@email.arizona.edu \
-       -e SMTP_ADMIN=glbrimhall@email.arizona.edu \
-       --link compose_db-host_1:db-host \
-       --name $CONTAINER \
-       $REPOSITORY:$TAG
-
-#       -v /etc/shibboleth:/etc/shibboleth \
-#       -p 9090:9090 \
-
-fi
+for SCRIPT in $DBUILD_SCRIPTS; do
+  echo "LAUNCHING $SCRIPT"
+  ./$SCRIPT $ACTION
+done
